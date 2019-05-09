f605db6f022ae9da95c78b3936f7a608d78726d1
Android Studio <devandroid@ort>
2019-05-09 12:11:05 -0300

    First commit

--- app/src/main/java/com/ducklings_corp/tp3/MainActivity.java
+++ app/src/main/java/com/ducklings_corp/tp3/MainActivity.java
@@ -3,11 +3,25 @@ package com.ducklings_corp.tp3;
 import android.app.Activity;
 import android.os.Bundle;
 
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        URL api;
+        HttpURLConnection connection;
+        try {
+            api = new URL("http://epok.buenosaires.gob.ar/getCategorias");
+            connection = (HttpURLConnection) api.openConnection();
+        } catch (Exception e) {
+
+        }
+
+        pri
     }
 }
